45d6da9ee28de5fe5e081bf641e9be63d1b4a0d3
kota-shiokara <ikanoshiokara.fun@gmail.com>
2020-10-18 14:17:09 +0900

    feat:Move YM to Y

--- Y2M/y2m.js
+++ Y2M/y2m.js
@@ -2,10 +2,14 @@ chrome.browserAction.onClicked.addListener(
     function(tab){
         var yturl = tab.url;
         console.log(tab.url);
-        if(yturl.indexOf('watch') != -1){
+        if(yturl.indexOf('www.youtube.com/watch') != -1){
             var result = yturl.substring(yturl.indexOf('watch'));
             if(result.indexOf('&') != -1) result = result.substring(0, result.indexOf('&'));
             window.open("https://music.youtube.com/" + result);
+        }else if(yturl.indexOf('music.youtube.com/watch') != -1){
+            var result = yturl.substring(yturl.indexOf('watch'));
+            if(result.indexOf('&') != -1) result = result.substring(0, result.indexOf('&'));
+            window.open("https://youtube.com/" + result);
         }else{
             alert("Move failed to youtube music");
         }
