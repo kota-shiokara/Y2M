682c31d20d947b789d67aeec6f419fff72f453f2
kota-shiokara <ikanoshiokara.fun@gmail.com>
2021-01-13 11:51:46 +0900

    feature share twitter

--- Y2M/y2m.js
+++ Y2M/y2m.js
@@ -3,13 +3,29 @@ chrome.browserAction.onClicked.addListener(
         var yturl = tab.url;
         console.log(tab.url);
         if(yturl.indexOf('www.youtube.com/watch') != -1){
+            // Youtubeでの処理
             var result = yturl.substring(yturl.indexOf('watch'));
             if(result.indexOf('&') != -1) result = result.substring(0, result.indexOf('&'));
+
             window.open("https://music.youtube.com/" + result);
         }else if(yturl.indexOf('music.youtube.com/watch') != -1){
-            var result = yturl.substring(yturl.indexOf('watch'));
-            if(result.indexOf('&') != -1) result = result.substring(0, result.indexOf('&'));
-            window.open("https://youtube.com/" + result);
+            // Youtube Musicでの処理
+            if(confirm("共有しますか")){
+                var tweetText = tab.title;
+                tweetText = tweetText.substring(0, tweetText.indexOf("- YouTube Music"));
+
+                var result = yturl.substring(yturl.indexOf('watch'));
+                if(result.indexOf('&') != -1) result = result.substring(0, result.indexOf('&'));
+
+                var tweetUrl = "https://youtube.com/" + result;
+
+                window.open("https://twitter.com/intent/tweet?text=" + tweetText + "&url=" + tweetUrl + "&via=YouTube");
+            }else{
+                var result = yturl.substring(yturl.indexOf('watch'));
+                if(result.indexOf('&') != -1) result = result.substring(0, result.indexOf('&'));
+
+                window.open("https://youtube.com/" + result);
+            }
         }else{
             alert("Move failed to youtube music");
         }
